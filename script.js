e9dd191560e73a6cb83d0ab2c53d2281f1e318d0
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2022-01-21 20:16:34 +0530

    Improved UI,Removed Unecessary CSS, Added New Components

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const navMenu = document.querySelector('nav')
+const toggleMenu = document.getElementById('toggle-menu')
+const closeMenu = document.getElementById('close-menu')
+
+toggleMenu.addEventListener('click', () => {
+    console.log("Nigger")
+    navMenu.classList.toggle('show');
+})
+closeMenu.addEventListener('click', () => {
+    console.log("Nigger")
+    navMenu.classList.toggle('show');
+})
+
 
